5f8d7c7fcd5a70bbcce5f429ed4d8453a680fa7a
Pritesh Vardekar <priteshv.264@gmail.com>
2023-12-08 01:05:24 +0530

    first commit

---
+++ js/responsive.js
@@ -0,0 +1,9 @@
+navbar = document.querySelector(".navbar");
+rightNav =document.querySelector(".rightNav");
+navlist = document.querySelector(".nav-list");
+function viewfunc(){
+     navbar.classList.toggle("nav-height-resp");
+     rightNav.classList.toggle("v-class-resp");
+     navlist.classList.toggle("v-class-resp");
+    
+}
